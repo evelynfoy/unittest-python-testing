0d34a82fd6da8b8ef11b56df9d61cd22c00ce5e9
evelynfoy <evelyn.foy@hotmail.com>
2022-04-12 17:46:27 +0000

    Add basic python test examples

---
+++ evens.py
@@ -0,0 +1,18 @@
+def even_numbers_of_evens(numbers):
+    """
+    Should Raise a TypeError if a list in not passed into the function
+    error message: "A list was not passed into the function"
+    if the list is empty it will return False
+    if the number of even numbers is odd - return False
+    if the numner of even numbers is even - return True
+    """
+    if isinstance(numbers, list):
+        evens = sum([1 for num in numbers if num % 2 == 0])
+
+        return True if evens and evens % 2 == 0 else False
+    else:
+        raise TypeError("A list was not pased into the function")
+
+
+if __name__ == '__main__':
+    print(even_numbers_of_evens([2,1,4]))
